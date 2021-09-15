823fdb21f03aebca49a9ed0f9b79a66bae87940c
HA DO THI <1821050898@student.humg.edu.vn>
2021-09-15 08:22:10 +0700

    Done

--- Controllers/HelloWorldControllers.cs
+++ Controllers/HelloWorldControllers.cs
@@ -8,17 +8,17 @@ namespace demoMVC.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View(); 
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
+        public IActionResult GetName()
         {
-            return "This is the Welcome action method...";
+            return View(); 
         }
     }
 }
